74dee6554a88d8bd618f9dd32c7950f892b7bf91
Adrian Roe <adrian@id3as.co.uk>
2019-11-20 17:36:00 +0000

    First simple test in place

---
+++ test/packages.dhall
@@ -0,0 +1,19 @@
+let upstream =
+      https://github.com/purescript/package-sets/releases/download/psc-0.13.4-20191110/packages.dhall sha256:563a7f694e18e6399f7f6d01f5b7e3c3345781655d99945768f48e458feb93a4
+
+let overrides = {=}
+
+let additions = 
+      { http =
+          { dependencies =
+              [
+              ] : List Text
+          , repo =
+              "ssh://git@github.com/joneshf/purescript-http.git"
+          , version =
+              "v4.0.0"
+          }
+      }
+
+
+in  upstream // overrides // additions
---
+++ test/src/Main.purs
@@ -0,0 +1,38 @@
+module Main where
+
+import Prelude
+
+import Affjax as AX
+import Affjax.ResponseFormat as ResponseFormat
+import Affjax.StatusCode (StatusCode(..))
+import Data.Argonaut.Core as J
+import Data.Either (Either(..))
+import Data.HTTP.Method (Method(..))
+import Data.Time.Duration (Milliseconds(..))
+import Debug.Trace (spy)
+import Effect (Effect)
+import Effect.Aff (launchAff_, delay)
+import Effect.Class.Console (log)
+import Test.Spec (pending, describe, it)
+import Test.Spec.Assertions (fail, shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
+
+main :: Effect Unit
+main =
+  launchAff_
+    $ runSpec [ consoleReporter ] do
+        describe "client requests stream" do
+          it "fails if no edge agent" do
+            result <- AX.request (AX.defaultRequest { url = "http://localhost:3000/poc/api/client/canary/foo", method = Left GET, responseFormat = ResponseFormat.string })
+            case spy "result is" result of
+               Left err -> do
+                 log $ "GET /api response failed to decode: " <> AX.printError err
+                 fail "HTTP Error"
+               Right response -> do
+                 -- log $ "GET /api response: " <> response.body
+                 case response.status of
+                   StatusCode 200 ->
+                     pure unit
+                   sc ->
+                     fail $ "Unexpected statuscode" <> show sc
