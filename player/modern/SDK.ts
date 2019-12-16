940473b73c1a13d8d5335e812adb2a7c51a4372f
Philip Stears <philip@philipstears.com>
2019-12-16 15:59:26 +0000

    Import player code.

---
+++ player/modern/SDK.ts
@@ -0,0 +1,48 @@
+import IPlayer from "./frontend/IPlayer";
+import IPlayerConfiguration from "./frontend/IPlayerConfiguration";
+
+import * as PlayerConfiguration from "./backend/PlayerConfiguration";
+import Player from "./backend/Player";
+
+// TODO: put the actual DNS name in here
+const DOMAIN_NAME: string = "rts.llnwi.net";
+const API_VERSION: string = "1.0";
+
+export function createPlayer(configuration: IPlayerConfiguration) {
+  const configurationWithDefaults = PlayerConfiguration.withDefaults(configuration);
+  createPlayerPrime(configurationWithDefaults);
+}
+
+function createPlayerPrime(configuration: IPlayerConfiguration) {
+
+  // NOTE: we pack all the info into the URI because that allows the edge
+  //       to do an HTTP-level redirect if it wants us to go somewhere else
+  const socketURL = socketUrl(configuration);
+
+  const videoElement =
+    <HTMLVideoElement>document.getElementById(configuration.videoElementId);
+
+  // TODO: Preflight checks?
+  return new Player(configuration.account, configuration.streamName, socketURL, videoElement);
+}
+
+function getOrCreateTraceId(): string {
+  return null;
+}
+
+function socketUrl(configuration: IPlayerConfiguration) {
+  const overrides = configuration.overrides || {};
+  const authority = overrides.socketAuthority || `${configuration.account}.${DOMAIN_NAME}`;
+  const scheme = socketScheme(overrides.socketSecure);
+
+  return `${scheme}://${authority}/play/${API_VERSION}/${configuration.account}/${configuration.streamName}`;
+}
+
+function socketScheme(secureOption) {
+  switch (secureOption) {
+    case false:
+      return "ws";
+    default:
+      return "wss";
+  }
+}
---
+++ player/modern/backend/PlayerConfiguration.ts
@@ -0,0 +1,22 @@
+import IPlayerConfiguration from "../frontend/IPlayerConfiguration";
+
+export function withDefaults(configuration: IPlayerConfiguration): IPlayerConfiguration {
+  return {
+    account: defaultValue(configuration.account, null),
+    streamName: defaultValue(configuration.streamName, null),
+    videoElementId: defaultValue(configuration.videoElementId, "llnw-rts-subscriber"),
+    autoLayoutOrientation: defaultValue(configuration.autoLayoutOrientation, true),
+    overrides: defaultOverrides(defaultValue(configuration.overrides, {})),
+  };
+}
+
+function defaultValue(value, defaultValue) {
+  return (value === undefined || value === null) ? defaultValue : value;
+}
+
+function defaultOverrides(overrides) {
+  return {
+    socketAuthority: defaultValue(overrides.socketAuthority, null),
+    socketSecure: defaultValue(overrides.socketSecure, null),
+  };
+}
