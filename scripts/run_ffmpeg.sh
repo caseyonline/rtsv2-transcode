9689c334a2dc3b09541133fc624bf77789d607ec
Steve Strong <steve@id3as.co.uk>
2020-01-22 14:30:38 +0000

    Missed file from last commit

--- scripts/run_ffmpeg.sh
+++ scripts/run_ffmpeg.sh
@@ -1,3 +1,5 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p ffmpeg-full
 file=$1
 ingestUrl=$2
 ffmpeg -re -i "$file" -ar 44100 -acodec aac -vcodec h264 -g 25 -preset medium -x264-params "threads=1:rc-lookahead=0" -vf drawtext="fontfile=monofonto.ttf: fontsize=96: box=1: boxcolor=black@0.75: boxborderw=5: fontcolor=white: x=(w-text_w)/2: y=((h-text_h)/2)+((h-text_h)/4): text='%{gmtime\:%H\\\\\:%M\\\\\:%S}'" -bf 0 -f flv "$ingestUrl pubUser=user pubPasswd=password"
