884ef18e149d929af30520043e45e80f5feffc00
Steve Strong <steve@id3as.co.uk>
2020-01-28 14:31:36 +0000

    Change nix_ssl_cert_file locations for llnw install

--- release-files/start_node_nix.sh
+++ release-files/start_node_nix.sh
@@ -4,6 +4,6 @@ cd ${0%/*}
 export PATH=/home/id3as/.nix-profile/bin:"$PATH"
 export NIX_PROFILES="/nix/var/nix/profiles/default /home/id3as/.nix-profile"
 export NIX_PATH=/home/id3as/.nix-defexpr/channels
-export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
+export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
 
 nix-shell --run ./start.sh
--- release-files/start_serf_nix.sh
+++ release-files/start_serf_nix.sh
@@ -5,6 +5,6 @@ read -r HOSTNAME PRIVATE_IFACE PUBLIC_IFACE DISK_LOG_ROOT <<< $(cat ~/rtsv2_envi
 export PATH=/home/id3as/.nix-profile/bin:"$PATH"
 export NIX_PROFILES="/nix/var/nix/profiles/default /home/id3as/.nix-profile"
 export NIX_PATH=/home/id3as/.nix-defexpr/channels
-export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
+export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
 
 nix-shell --run "serf agent -iface $PRIVATE_IFACE -node $HOSTNAME -bind 127.0.0.1:7946 -rpc-addr 127.0.0.1:7373 | tee -a $DISK_LOG_ROOT/i-serf.log"
