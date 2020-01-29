3e94230263f14811f2ab553a500877884690d2b5
Steve Strong <steve@id3as.co.uk>
2020-01-29 17:23:34 +0000

    Fixed transpop start on osx

--- priv/scripts/startTransPoPAgent.sh
+++ priv/scripts/startTransPoPAgent.sh
@@ -4,7 +4,17 @@ cd ${0%/*}
 cd ..
 
 function main {
-  IP=$(ip -4  -o addr | grep \ $PRIVATE_IFACE\  | awk {'print $4'} | sed 's/\/.*$//')
+  if [[ $(uname) == "Darwin" ]]; then
+    IP=$(ifconfig $PRIVATE_IFACE | awk '/inet/ {print $2}')
+  else
+    IP=$(ip -4  -o addr | grep \ $PRIVATE_IFACE\  | awk {'print $4'} | sed 's/\/.*$//')
+  fi
+
+  echo serf agent \
+        -iface $PRIVATE_IFACE \
+        -node $HOSTNAME \
+        -bind $IP:8946 \
+        -rpc-addr $IP:8373 >> $DISK_LOG_ROOT/t-serf.log &
 
   nohup serf agent \
         -iface $PRIVATE_IFACE \
