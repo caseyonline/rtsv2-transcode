4a2eff8e923de13e35cd310ea10af8c176e42161
Steve Strong <steve@id3as.co.uk>
2020-02-20 10:40:48 +0000

    WIP on rtmp crypto contexts

--- server/src/Rtsv2/Agents/IngestRtmpCrypto.purs
+++ server/src/Rtsv2/Agents/IngestRtmpCrypto.purs
@@ -1,121 +1,191 @@
 module Rtsv2.Agents.IngestRtmpCrytpo
-       (
-         startLink
+       ( startLink
        )
        where
 
 import Prelude
 
+import Data.FoldableWithIndex (foldlWithIndex)
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Tuple (Tuple(..))
+import Data.Newtype (wrap)
 import Effect (Effect)
 import Erl.Data.List (List, filter, nil, null, (:))
-import Erl.Data.Map (Map, alter, lookup)
+import Erl.Data.Map (Map, alter, insert, lookup)
 import Erl.Data.Map as Map
+import Erl.Data.Tuple (Tuple2, fst, snd, tuple2)
 import Erl.Utils (vmTimeMs)
 import Pinto (ServerName)
 import Pinto as Pinto
 import Pinto.Gen (CallResult(..), CastResult(..))
 import Pinto.Gen as Gen
 import Pinto.Timer as Timer
+import Rtsv2.Config as Config
 import Rtsv2.Names as Names
 import Rtsv2.Utils (cryptoStrongToken, member)
 import Shared.Types (Milliseconds)
 
-serverName :: ServerName State Msg
-serverName = Names.ingestRtmpCryptoName
+type Username = String
 
-startLink :: forall a. a -> Effect Pinto.StartLinkResult
-startLink args = Gen.startLink serverName (init args) handleInfo
+type AdobeContext =
+  { salt :: String
+  , challenge :: String
+  }
 
-getSaltAndChallenge :: String -> Effect (Tuple String String)
-getSaltAndChallenge username =
-  Gen.doCall serverName \state@{contexts} -> do
-    salt <- cryptoStrongToken 6
-    challenge <- cryptoStrongToken 6
-    now <- vmTimeMs
-    let
-      context = SaltAndChallenge salt challenge
-      contextWithExpiry = Tuple now context
-      newContexts = alter (addToContext contextWithExpiry) username contexts
-    pure $ CallReply (Tuple salt challenge) state{contexts = newContexts}
+type LlnwContext =
+  { nonce :: String
+  }
 
-getNonce :: String -> Effect String
-getNonce username =
-  Gen.doCall serverName \state@{contexts} -> do
-    nonce <- cryptoStrongToken 6
-    now <- vmTimeMs
-    let
-      context = Nonce nonce
-      contextWithExpiry = Tuple now context
-      newContexts = alter (addToContext contextWithExpiry) username contexts
-    pure $ CallReply nonce state{contexts = newContexts}
+type ContextWithExpiry a = Tuple2 Milliseconds a
+type ContextsWithExpiry a = List (ContextWithExpiry a)
 
-getContexts :: String -> Effect (List Context)
-getContexts username =
-  Gen.call serverName \state@{contexts} ->
-  let
-    response = lookup username contexts
-               # fromMaybe nil
-              <#> (\(Tuple _ context) -> context)
-  in
-   CallReply response state
-
-consumeContext :: String -> Context -> Effect Boolean
-consumeContext username consumedContext =
-  Gen.call serverName \state@{contexts} ->
-  let
-    currentUserContexts = lookup username contexts
-                          # fromMaybe nil
-                          <#> (\(Tuple _ context) -> context)
-
-    removeContext Nothing = Nothing
-    removeContext (Just contextsWithExpiry) =
-      let
-        newContextsWithExpiry :: List ContextWithExpiry
-        newContextsWithExpiry = filter (\(Tuple _ context) -> context /= consumedContext) contextsWithExpiry
-      in
-       if null newContextsWithExpiry then Nothing
-       else Just newContextsWithExpiry
-
-
-    newContexts = alter removeContext username contexts
-  in
-   if
-     (member consumedContext currentUserContexts) then CallReply true state{contexts = newContexts}
-     else CallReply false state
-
-data Context = SaltAndChallenge String String
-             | Nonce String
-
-derive instance eqContext :: Eq Context
-
-type ContextWithExpiry = Tuple Milliseconds Context
-
-type UserContexts = List ContextWithExpiry
+type ContextLens a =
+  { get :: State -> Map Username (ContextsWithExpiry a)
+  , set :: Map Username (ContextsWithExpiry a) -> State -> State
+  }
 
 type State =
-  {
-    contexts :: Map String UserContexts
+  { adobeContexts :: Map Username (ContextsWithExpiry AdobeContext)
+  , llnwContexts ::  Map Username (ContextsWithExpiry LlnwContext)
+  , cryptoContextExpiry :: Milliseconds
   }
 
 data Msg =
   CheckExpiry
+
+serverName :: ServerName State Msg
+serverName = Names.ingestRtmpCryptoName
+
+------------------------------------------------------------------------------
+-- API
+------------------------------------------------------------------------------
+startLink :: forall a. a -> Effect Pinto.StartLinkResult
+startLink args = Gen.startLink serverName (init args) handleInfo
+
+newAdobeContext :: Username -> Effect AdobeContext
+newAdobeContext username =
+  Gen.doCall serverName \state -> do
+    salt <- cryptoStrongToken 6
+    challenge <- cryptoStrongToken 6
+    let
+      context = {salt, challenge}
+    addContextToStateAndReply username context adobeLens state
+
+newLlnwContext :: Username -> Effect LlnwContext
+newLlnwContext username =
+  Gen.doCall serverName \state@{llnwContexts} -> do
+    nonce <- cryptoStrongToken 6
+    let
+      context = {nonce}
+    addContextToStateAndReply username context llnwLens state
+
+getAdobeContexts :: Username -> Effect (List AdobeContext)
+getAdobeContexts username =
+  Gen.call serverName (getContexts username adobeLens)
+
+getLlnwContexts :: Username -> Effect (List LlnwContext)
+getLlnwContexts username =
+  Gen.call serverName (getContexts username llnwLens)
+
+consumeAdobeContext :: Username -> AdobeContext -> Effect Boolean
+consumeAdobeContext username consumedContext =
+  Gen.call serverName (consumeContext username consumedContext adobeLens)
+
+consumeLlnwContext :: Username -> LlnwContext -> Effect Boolean
+consumeLlnwContext username consumedContext =
+  Gen.call serverName (consumeContext username consumedContext llnwLens)
 
 ------------------------------------------------------------------------------
 -- Internals
 ------------------------------------------------------------------------------
 init :: forall a. a -> Effect State
 init _ = do
-  void $ Timer.sendAfter serverName 1000 CheckExpiry
-  pure $ { contexts: Map.empty
+  {cryptoContextExpiryMs} <- Config.rtmpIngestConfig
+  void $ Timer.sendAfter serverName cryptoContextExpiryMs CheckExpiry
+  pure $ { adobeContexts: Map.empty
+         , llnwContexts: Map.empty
+         , cryptoContextExpiry: wrap cryptoContextExpiryMs
          }
 
 handleInfo :: Msg -> State -> Effect (CastResult State)
 handleInfo msg state = case msg of
-  CheckExpiry ->
-    pure $ CastNoReply state
+  CheckExpiry -> do
+    state2 <- expireOldContexts state
+    pure $ CastNoReply state2
 
-addToContext :: ContextWithExpiry -> Maybe UserContexts -> Maybe UserContexts
-addToContext contextWithExpiry Nothing = Just (contextWithExpiry : nil)
-addToContext contextWithExpiry (Just existing) = Just (contextWithExpiry : existing)
+expireOldContexts :: State -> Effect State
+expireOldContexts state@{adobeContexts, llnwContexts} = do
+  now <- vmTimeMs
+  let
+    expireOld :: forall a. Username -> Map Username (ContextsWithExpiry a) -> ContextsWithExpiry a -> Map Username (ContextsWithExpiry a)
+    expireOld username acc userContexts =
+      let
+        newUserContexts = filter (\contextWithExpiry -> (fst contextWithExpiry) > now) userContexts
+      in
+       if null newUserContexts then acc
+       else insert username newUserContexts acc
+
+    adobeContexts2 = foldlWithIndex expireOld Map.empty adobeContexts
+    llnwContexts2 = foldlWithIndex expireOld Map.empty llnwContexts
+  pure state{ adobeContexts = adobeContexts2
+            , llnwContexts = llnwContexts2}
+
+addContextToStateAndReply :: forall a. Username -> a -> ContextLens a -> State -> Effect (CallResult a State)
+addContextToStateAndReply username context {get, set} state@{cryptoContextExpiry} = do
+  now <- vmTimeMs
+  let
+    contextWithExpiry = tuple2 (now + cryptoContextExpiry) context
+    contexts = get state
+    newContexts = alter (addToUserContexts contextWithExpiry) username contexts
+  pure $ Gen.CallReply context (set newContexts state)
+  where
+    addToUserContexts contextWithExpiry Nothing = Just (contextWithExpiry : nil)
+    addToUserContexts contextWithExpiry (Just existing) = Just (contextWithExpiry : existing)
+
+getContexts :: forall a. Username -> ContextLens a -> State -> CallResult (List a) State
+getContexts username {get} state =
+  let
+    result = get state
+             # getContexts' username
+  in
+   CallReply result state
+
+getContexts' :: forall a. Username -> Map Username (ContextsWithExpiry a) -> List a
+getContexts' username contexts =
+  lookup username contexts
+  # fromMaybe nil
+  <#> snd
+
+consumeContext :: forall a. Eq a => Username -> a -> ContextLens a -> State -> CallResult Boolean State
+consumeContext username consumedContext {get, set} state =
+  let
+    contexts = get state
+
+    currentUserContexts = getContexts' username contexts
+
+    removeContext Nothing = Nothing
+    removeContext (Just contextsWithExpiry) =
+      let
+        newContextsWithExpiry :: List (ContextWithExpiry a)
+        newContextsWithExpiry = filter (\contextWithExpiry -> (snd contextWithExpiry) /= consumedContext) contextsWithExpiry
+      in
+       if null newContextsWithExpiry then Nothing
+       else Just newContextsWithExpiry
+
+    newContexts = alter removeContext username contexts
+  in
+   if
+     (member consumedContext currentUserContexts) then CallReply true (set newContexts state)
+     else CallReply false state
+
+
+adobeLens :: ContextLens AdobeContext
+adobeLens =
+  { get: _.adobeContexts
+  , set: \newAdobeContexts state -> state{adobeContexts = newAdobeContexts}
+  }
+
+llnwLens :: ContextLens LlnwContext
+llnwLens =
+  { get: _.llnwContexts
+  , set: \newLlnwContexts state -> state{llnwContexts = newLlnwContexts}
+  }
