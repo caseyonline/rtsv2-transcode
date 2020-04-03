ba5a9eb2b0bd0c43500ff810eefd3d325bd66d80
philipstears <philip@philipstears.com>
2020-04-03 08:56:26 +0100

    Test Scripts: use nvenc for H.264 encode on my main desktop.

--- scripts/run_slot1_500.sh
+++ scripts/run_slot1_500.sh
@@ -11,9 +11,9 @@ ffmpeg \
   "${MEDIA_FILE_1000}" \
   -ar 48000 \
   -acodec aac \
-  -vcodec h264 \
+  -vcodec "${VIDEO_CODEC}" \
   -g 25 \
-  -preset faster \
+  -preset "${VIDEO_PRESET}" \
   -b 500k \
   -vf drawtext="fontfile=monofonto.ttf: fontsize=96: box=1: boxcolor=black@0.75: boxborderw=5: fontcolor=white: x=(w-text_w)/2: y=((h-text_h)/2)+((h-text_h)/4): text='LOW %{gmtime\:%H\\\\\:%M\\\\\:%S}'" \
   -bf 0 \
