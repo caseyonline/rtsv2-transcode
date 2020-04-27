868179551b79c2bf70c819f67b5d4ca691bacda7
Steve Strong <steve@id3as.co.uk>
2020-04-27 16:19:11 +0100

    WIP on load

--- server/src/Rtsv2/Load.purs
+++ server/src/Rtsv2/Load.purs
@@ -7,13 +7,16 @@ module Rtsv2.Load
 import Prelude
 import Rtsv2.LoadTypes
 
+import Data.Foldable (foldl)
 import Data.Int (round, toNumber)
-import Data.Newtype (wrap)
+import Data.Maybe (Maybe(..))
+import Data.Newtype (unwrap, wrap)
+import Data.Set (fromFoldable)
+import Data.Set as Set
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Erl.Atom (Atom)
 import Erl.Data.List (List, singleton)
-import Erl.Data.Tuple (uncurry2)
 import Logger (Logger)
 import Logger as Logger
 import Pinto (ServerName, StartLinkResult)
@@ -23,79 +26,38 @@ import Pinto.Timer as Timer
 import Rtsv2.Agents.IntraPoP as IntraPop
 import Rtsv2.Config (LoadConfig)
 import Rtsv2.Config as Config
-import Rtsv2.Env as Env
 import Rtsv2.Names as Names
 import Shared.Rtsv2.Agent (AggregatorSerfPayload)
-import Shared.Rtsv2.Types (Load, NetworkBPS(..), Percentage(..), SpecInt(..))
-
--- decaying things
--- monitor actual load - cpu %age and network bps
-
+import Shared.Rtsv2.Types (Load, NetworkBPS(..), Percentage(..), Server(..), SpecInt(..))
 
 type CurrentLoad =
   { currentCPU :: Percentage
   , currentNetwork :: NetworkBPS
   }
 
-hasCapacityForEgestInstance :: AggregatorSerfPayload -> CurrentLoad -> LoadConfig -> Boolean
-hasCapacityForEgestInstance serfPayload currentLoad loadConfig = false
+hasCapacityForEgestInstance :: AggregatorSerfPayload -> Server -> CurrentLoad -> LoadConfig -> LoadCheckResult
+hasCapacityForEgestInstance serfPayload targetServer currentLoad {costs: LoadCosts {egest: agentCosts}, limits} =
+  hasCapacity serfPayload targetServer currentLoad agentCosts limits
 
-hasCapacityForEgestClient :: AggregatorSerfPayload -> CurrentLoad -> LoadConfig -> Boolean
-hasCapacityForEgestClient serfPayload currentLoad loadConfig = false
+hasCapacityForEgestClient :: AggregatorSerfPayload -> Server -> CurrentLoad -> LoadConfig -> LoadCheckResult
+hasCapacityForEgestClient serfPayload targetServer currentLoad {costs: LoadCosts {egest: agentCosts}, limits} =
+  hasCapacity serfPayload targetServer currentLoad agentCosts limits
 
-hasCapacityForStreamRelay :: AggregatorSerfPayload -> CurrentLoad -> LoadConfig -> Boolean
-hasCapacityForStreamRelay serfPayload currentLoad loadConfig = false
+hasCapacityForStreamRelay :: AggregatorSerfPayload -> Server -> CurrentLoad -> LoadConfig -> LoadCheckResult
+hasCapacityForStreamRelay serfPayload targetServer currentLoad {costs: LoadCosts {streamRelay: agentCosts}, limits} =
+  hasCapacity serfPayload targetServer currentLoad agentCosts limits
 
-hasCapacityForAggregator :: AggregatorSerfPayload -> CurrentLoad -> LoadConfig -> Boolean
-hasCapacityForAggregator serfPayload currentLoad loadConfig = false
+hasCapacityForAggregator :: AggregatorSerfPayload -> Server -> CurrentLoad -> LoadConfig -> LoadCheckResult
+hasCapacityForAggregator serfPayload targetServer currentLoad {costs: LoadCosts {ingestAggregator: agentCosts}, limits} =
+  hasCapacity serfPayload targetServer currentLoad agentCosts limits
 
-hasCapacityForRtmpIngest :: AggregatorSerfPayload -> CurrentLoad -> LoadConfig -> Boolean
-hasCapacityForRtmpIngest serfPayload currentLoad loadConfig = false
+hasCapacityForRtmpIngest :: AggregatorSerfPayload -> Server -> CurrentLoad -> LoadConfig -> LoadCheckResult
+hasCapacityForRtmpIngest serfPayload targetServer currentLoad {costs: LoadCosts {webRTCIngest: agentCosts}, limits} =
+  hasCapacity serfPayload targetServer currentLoad agentCosts limits
 
-hasCapacityForWebRTCIngest :: AggregatorSerfPayload -> CurrentLoad -> LoadConfig -> LoadCheckResult
-hasCapacityForWebRTCIngest serfPayload currentLoad config@{costs: LoadCosts {webRTCIngest}} =
-  hasCapacity (agentCostsToFixed webRTCIngest serfPayload) currentLoad config
-
-hasCapacity :: LoadFixedCosts -> CurrentLoad -> LoadConfig -> LoadCheckResult
-hasCapacity (LoadFixedCosts { cpu: additionalCpu
-                            , network: additionalNetwork }) { currentCPU
-                                                            , currentNetwork } { limits: LoadLimits {cpu: cpuLimits, network: networkLimits}} =
-  let
-    -- TODO needs to get passed a server so that it know capacity, hardware tags etc
-    maxCpuCapacity = wrap 1.0
-    maxNetworkCapacity = wrap 1
-    proposedCpu = currentCPU <> (cpuUnitsToPercentage additionalCpu maxCpuCapacity)
-    proposedNetwork = networkToPercentage (currentNetwork <> additionalNetwork) maxNetworkCapacity
-    cpuResult = checkWatermark proposedCpu cpuLimits
-    networkResult = checkWatermark proposedNetwork networkLimits
-  in
-   max cpuResult networkResult
-
-checkWatermark :: Percentage -> LoadWatermarks -> LoadCheckResult
-checkWatermark percentage (LoadWatermarks {lowWaterMark, highWaterMark}) =
-  if percentage < lowWaterMark then Green
-  else if percentage < highWaterMark then Amber
-       else Red
-
-agentCostsToFixed :: LoadAgentCosts -> AggregatorSerfPayload -> LoadFixedCosts
-agentCostsToFixed (LoadAgentCosts {fixed, variable}) streamCosts =
-  fixed <> (variableCostsToFixed variable streamCosts)
-
-variableCostsToFixed :: LoadVariableCosts -> AggregatorSerfPayload -> LoadFixedCosts
-variableCostsToFixed (LoadVariableCosts {cpu, network}) streamCosts =
-  LoadFixedCosts { cpu: SpecInt $ variableCostToFixed cpu streamCosts
-                 , network: NetworkBPS $ round $ variableCostToFixed network streamCosts
-                 }
-
-variableCostToFixed :: LoadVariableCost -> AggregatorSerfPayload -> Number
-variableCostToFixed (LoadVariableCost variable) (Tuple numStreams totalBitrate) =
-  uncurry2 (\numStreamFactor bitrateFactor -> (numStreamFactor * (toNumber numStreams)) + (bitrateFactor * (toNumber totalBitrate))) variable
-
-cpuUnitsToPercentage :: SpecInt -> SpecInt -> Percentage
-cpuUnitsToPercentage (SpecInt x) (SpecInt y) = Percentage (x * 100.0 / y)
-
-networkToPercentage :: NetworkBPS -> NetworkBPS -> Percentage
-networkToPercentage (NetworkBPS x) (NetworkBPS y) = Percentage ((toNumber x) * 100.0 / (toNumber y))
+hasCapacityForWebRTCIngest :: AggregatorSerfPayload -> Server -> CurrentLoad -> LoadConfig -> LoadCheckResult
+hasCapacityForWebRTCIngest serfPayload targetServer currentLoad {costs: LoadCosts {rtmpIngest: agentCosts}, limits} =
+  hasCapacity serfPayload targetServer currentLoad agentCosts limits
 
 type State =
   {
@@ -134,6 +96,83 @@ handleInfo msg state@{load: currentLoad} =
       do
         IntraPop.announceLoad currentLoad
         pure $ CastNoReply state
+
+--------------------------------------------------------------------------------
+-- Internals
+--------------------------------------------------------------------------------
+hasCapacity :: AggregatorSerfPayload -> Server -> CurrentLoad -> LoadAgentCosts -> LoadLimits -> LoadCheckResult
+hasCapacity serfPayload@(Tuple numStreams bitrate) targetServer currentLoad agentCosts limits =
+  let
+    {currentCPU, currentNetwork} = currentLoad
+    Server {maxCpuCapacity, maxNetworkCapacity} = targetServer
+    LoadLimits {cpu: cpuLimits, network: networkLimits} = limits
+
+    LoadFixedCost {cpu: additionalCpu, network: additionalNetwork} = agentCostsToFixed serfPayload targetServer agentCosts
+    proposedCpu = currentCPU <> (cpuUnitsToPercentage additionalCpu maxCpuCapacity)
+    proposedNetwork = networkToPercentage (currentNetwork <> additionalNetwork) maxNetworkCapacity
+    cpuResult = checkWatermark proposedCpu cpuLimits
+    networkResult = checkWatermark proposedNetwork networkLimits
+  in
+   max cpuResult networkResult
+
+agentCostsToFixed :: AggregatorSerfPayload -> Server -> LoadAgentCosts -> LoadFixedCost
+agentCostsToFixed serfPayload@(Tuple numStreams bitrate) targetServer agentCosts =
+  let
+    LoadAgentCosts {fixed, perStream, perBPS, hardwareFactors} = agentCosts
+    perStreamFixed = perStreamToFixed perStream numStreams
+    perBitrateFixed = perBitrateToFixed perBPS bitrate
+    standardFixed = fixed <> perStreamFixed <> perBitrateFixed
+  in
+    adjustForHardwareCapabilites standardFixed targetServer hardwareFactors
+
+perStreamToFixed :: LoadVariableCost -> Int -> LoadFixedCost
+perStreamToFixed variable numStreams =
+  let
+    LoadVariableCost {cpu, network} = variable
+  in
+    LoadFixedCost { cpu: wrap $ (unwrap cpu) * (toNumber numStreams)
+                  , network: wrap $ (unwrap network) * numStreams
+                  }
+
+perBitrateToFixed :: LoadVariableCost -> Int -> LoadFixedCost
+perBitrateToFixed variable bitrate =
+  let
+    LoadVariableCost {cpu, network} = variable
+  in
+    LoadFixedCost { cpu: wrap $ (unwrap cpu) * (toNumber bitrate)
+                  , network: wrap $ (unwrap network) * bitrate
+                  }
+
+adjustForHardwareCapabilites :: LoadFixedCost -> Server -> List HardwareFactor -> LoadFixedCost
+adjustForHardwareCapabilites initialCost (Server {capabilityTags}) hardwareFactors =
+  foldl applyHardwareCapability initialCost hardwareFactors
+  where
+    applyHardwareCapability cost (HardwareFactor {name, cpuFactor, networkFactor}) =
+      case Set.member name serverSet of
+        false -> cost
+        true ->
+          applyNetworkFactor networkFactor $ applyCpuFactor cpuFactor cost
+
+    serverSet = fromFoldable capabilityTags
+
+    applyNetworkFactor Nothing cost = cost
+    applyNetworkFactor (Just factor) (LoadFixedCost cost@{network}) = LoadFixedCost cost{network = wrap $ round $ ((toNumber <<< unwrap) network) * factor}
+
+    applyCpuFactor Nothing cost = cost
+    applyCpuFactor (Just factor) (LoadFixedCost cost@{cpu}) = LoadFixedCost cost{cpu = wrap $ (unwrap cpu) * factor}
+
+checkWatermark :: Percentage -> LoadWatermarks -> LoadCheckResult
+checkWatermark percentage (LoadWatermarks {lowWaterMark, highWaterMark}) =
+  if percentage < lowWaterMark then Green
+  else if percentage < highWaterMark then Amber
+       else Red
+
+cpuUnitsToPercentage :: SpecInt -> SpecInt -> Percentage
+cpuUnitsToPercentage (SpecInt x) (SpecInt y) = Percentage (x * 100.0 / y)
+
+networkToPercentage :: NetworkBPS -> NetworkBPS -> Percentage
+networkToPercentage (NetworkBPS x) (NetworkBPS y) = Percentage ((toNumber x) * 100.0 / (toNumber y))
+
 
 --------------------------------------------------------------------------------
 -- Log helpers
--- server/src/Rtsv2/LoadTypes.purs
+++ server/src/Rtsv2/LoadTypes.purs
@@ -2,23 +2,20 @@ module Rtsv2.LoadTypes
  ( LoadAgentCosts(..)
  , LoadCheckResult(..)
  , LoadVariableCost(..)
- , LoadVariableCosts(..)
- , LoadFixedCosts(..)
+ , LoadFixedCost(..)
  , LoadLimits(..)
  , LoadCosts(..)
  , LoadWatermarks(..)
+ , HardwareFactor(..)
  ) where
 
 import Prelude
 
-import Control.Apply (lift2)
+import Data.Maybe (Maybe)
 import Data.Newtype (class Newtype, unwrap, wrap)
-import Erl.Data.Tuple (Tuple2, tuple2, uncurry2)
-import Erl.Utils as Erl
-import Foreign (F, Foreign)
+import Erl.Data.List (List)
 import Shared.Rtsv2.Types (NetworkBPS, Percentage, SpecInt)
-import Simple.JSON (class ReadForeign, read')
-
+import Simple.JSON (class ReadForeign)
 
 data LoadCheckResult = Green
                      | Amber
@@ -28,18 +25,23 @@ newtype LoadWatermarks = LoadWatermarks { lowWaterMark :: Percentage
                                         , highWaterMark :: Percentage
                                         }
 
-newtype LoadFixedCosts = LoadFixedCosts { cpu :: SpecInt
-                                        , network :: NetworkBPS
+newtype LoadFixedCost = LoadFixedCost { cpu :: SpecInt
+                                      , network :: NetworkBPS
+                                      }
+
+newtype LoadVariableCost = LoadVariableCost { cpu :: SpecInt
+                                            , network :: NetworkBPS
+                                            }
+
+newtype HardwareFactor = HardwareFactor { name :: String
+                                        , cpuFactor :: Maybe Number
+                                        , networkFactor :: Maybe Number
                                         }
 
-newtype LoadVariableCost = LoadVariableCost (Tuple2 Number Number)
-
-newtype LoadVariableCosts = LoadVariableCosts { cpu :: LoadVariableCost
-                                              , network :: LoadVariableCost
-                                              }
-
-newtype LoadAgentCosts = LoadAgentCosts { fixed :: LoadFixedCosts
-                                        , variable :: LoadVariableCosts
+newtype LoadAgentCosts = LoadAgentCosts { fixed :: LoadFixedCost
+                                        , perStream :: LoadVariableCost
+                                        , perBPS :: LoadVariableCost
+                                        , hardwareFactors :: List HardwareFactor
                                         }
 
 newtype LoadLimits = LoadLimits { network :: LoadWatermarks
@@ -56,6 +58,9 @@ newtype LoadCosts = LoadCosts { rtmpIngest :: LoadAgentCosts
 ------------------------------------------------------------------------------
 -- Type classes
 ------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+-- LoadCheckResult
 derive instance eqLoadCheckResult :: Eq LoadCheckResult
 instance ordLoadCheckResult :: Ord LoadCheckResult where
   compare Green Green = EQ
@@ -66,35 +71,41 @@ instance ordLoadCheckResult :: Ord LoadCheckResult where
   compare _ Amber = GT
   compare Red Red = EQ
 
+------------------------------------------------------------------------------
+-- LoadWatermarks
 derive instance newtypeLoadWatermarks :: Newtype LoadWatermarks _
 derive newtype instance readForeignLoadWatermarks :: ReadForeign LoadWatermarks
 
+------------------------------------------------------------------------------
+-- LoadLimits
 derive instance newtypeLoadLimits :: Newtype LoadLimits _
 derive newtype instance readForeignLoadLimits :: ReadForeign LoadLimits
 
+------------------------------------------------------------------------------
+-- LoadCosts
 derive instance newtypeLoadCosts :: Newtype LoadCosts _
 derive newtype instance readForeignLoadCosts :: ReadForeign LoadCosts
 
+------------------------------------------------------------------------------
+-- HardwareFactor
+derive instance newtypeHardwareFactor :: Newtype HardwareFactor _
+derive newtype instance readForeignHardwareFactor :: ReadForeign HardwareFactor
+
+------------------------------------------------------------------------------
+-- LoadAgentsCosts
 derive instance newtypeLoadAgentCosts :: Newtype LoadAgentCosts _
 derive newtype instance readForeignLoadAgentCosts :: ReadForeign LoadAgentCosts
 
-derive instance newtypeLoadFixedCosts :: Newtype LoadFixedCosts _
-derive newtype instance readForeignLoadFixedCosts :: ReadForeign LoadFixedCosts
+------------------------------------------------------------------------------
+-- LoadFixedCost
+derive instance newtypeLoadFixedCost :: Newtype LoadFixedCost _
+derive newtype instance readForeignLoadFixedCost :: ReadForeign LoadFixedCost
 
-derive instance newtypeLoadVariableCosts :: Newtype LoadVariableCosts _
-derive newtype instance readForeignLoadVariableCosts :: ReadForeign LoadVariableCosts
+instance semigroupLoadFixedCost :: Semigroup LoadFixedCost where
+  append (LoadFixedCost {cpu: cpuX, network: networkX}) (LoadFixedCost {cpu: cpuY, network: networkY}) =
+    LoadFixedCost {cpu: wrap (unwrap cpuX + unwrap cpuY), network: wrap (unwrap networkX + unwrap networkY)}
 
-instance readForeignLoadVariableCost :: ReadForeign LoadVariableCost where
-  readImpl o =
-    (uncurry2 toLoadVariable) =<< (Erl.readTuple2 o)
-    where
-      toLoadVariable :: Foreign -> Foreign -> F LoadVariableCost
-      toLoadVariable f1 f2 =
-        LoadVariableCost <$> lift2 tuple2 (readNumber f1) (readNumber f2)
-      readNumber :: Foreign -> F Number
-      readNumber f = read' f
-
-
-instance semigroupLoadFixedCosts :: Semigroup LoadFixedCosts where
-  append (LoadFixedCosts {cpu: cpuX, network: networkX}) (LoadFixedCosts {cpu: cpuY, network: networkY}) =
-    LoadFixedCosts {cpu: wrap (unwrap cpuX + unwrap cpuY), network: wrap (unwrap networkX + unwrap networkY)}
+------------------------------------------------------------------------------
+-- LoadVariableCost
+derive instance newtypeLoadVariableCost :: Newtype LoadVariableCost _
+derive newtype instance readForeignLoadVariableCost :: ReadForeign LoadVariableCost
