eb9f6172638bbbc2ac8732317194f9a42e97947a
Cmdv <vincent@cmdv.me>
2020-05-21 09:15:17 +0100

    move css over to it's own file

--- test/src/Cases/BrowserDataMsging.purs
+++ test/src/Cases/BrowserDataMsging.purs
@@ -51,40 +51,46 @@ ingestStream =
       after_ (F.stopSession *> F.stopSlot) do
         it "5.3.1 webrtc ingest on different node to aggregator" do
 
-          -- traverse_ F.maxOut (F.allNodesBar E.p1n1 ingestNodes) >>= L.as' "load up all servers bar one"
-          E.waitForIntraPoPDisseminate
-          -- F.startSlotHigh1000 (C.toAddrFromNode Env.p1n1) >>= L.as' "create high ingest on 1"
-          -- F.startSlotHigh1000Backup (C.toAddrFromNode Env.p1n1) >>= L.as' "create high backup on 1"
+          F.startSlotHigh1000 (C.toAddrFromNode Env.p1n1) >>= L.as' "create high ingest on 1"
+          F.startSlotHigh1000Backup (C.toAddrFromNode Env.p1n1) >>= L.as' "create high backup on 1"
           F.start2SlotHigh1000 (C.toAddrFromNode Env.p2n1) >>= L.as' "create high ingest on 2"
 
           _ <- delay (Milliseconds 5000.00) >>= L.as' "wait for ingest to start fully"
 
           browser1 <- T.launch options
+
           tab1 <- T.newPage browser1
           tab2 <- T.newPage browser1
           tab3 <- T.newPage browser1
+          tab4 <- T.newPage browser1
 
-          -- T.goto (F.mkPlayerUrl Env.p1n1 E.slot1 Primary) tab1
-          -- T.goto (F.mkPlayerUrl Env.p1n2 E.slot1 Primary) tab2
+          T.goto (F.mkPlayerUrl Env.p1n1 E.slot1 Primary) tab1
+          T.goto (F.mkPlayerUrl Env.p1n2 E.slot1 Primary) tab2
           T.goto (F.mkPlayerUrl Env.p2n1 E.slot2 Primary) tab3
-          _ <- delay (Milliseconds 60000.00) >>= L.as' "wait for webRTC connection"
+          T.goto (HTTP.ingestUrl Env.p1n1 E.shortName1 E.highStreamName) tab4
 
-          -- traceIds aren't showing on UI in tests
-          -- traceId1 <- F.getInnerText "#traceId" tab1
-          -- traceId2 <- F.getInnerText "#traceId" tab2
+          _ <- delay (Milliseconds 2000.00) >>= L.as' "wait for tabs to load"
+          traceId1 <- F.getInnerText "#traceId" tab1
+          traceId2 <- F.getInnerText "#traceId" tab2
+          traceId3 <- F.getInnerText "#traceId" tab3
+          -- traceId4 <- F.getInnerText "#traceId" tab4
 
+          -- go to tab2 send a message
+          T.bringToFront tab2
           T.focus (T.Selector "input#msginput") tab2
           T.keyboardSendCharacter "Hello World" tab2
           T.click (T.Selector "button#msgsend") tab2
 
           _ <- delay (Milliseconds 250.00) >>= L.as' "wait message to send"
-          -- need to change page
+          msgSentTraceId <- F.getInnerText "div.message.msg_sent > div.msg_name" tab2
+
+          -- Go to tab 1 check message was sent
           T.bringToFront tab1
           T.focus (T.Selector "input#msginput") tab1
           T.keyboardSendCharacter "Oh hi World" tab1
           T.click (T.Selector "button#msgsend") tab1
 
-          msgSentTraceId <- F.getInnerText "div.message.msg_sent > div.msg_name" tab2
+          _ <- delay (Milliseconds 60000.00) >>= L.as' "wait for webRTC connection"
           msgReceivedTraceId <- F.getInnerText "div.message.msg_received > div.msg_name" tab1
 
 
