ab6143042f9f4470b40e4caf55cf6cb284a31ad0
Steve Strong <steve@id3as.co.uk>
2020-05-21 14:07:17 +0100

    And added the supervisor itself :)

---
+++ server/src/Rtsv2/Agents/IngestOneForOneSup.purs
@@ -0,0 +1,44 @@
+module Rtsv2.Agents.IngestOneForOneSup
+  ( startLink
+  ) where
+
+import Prelude
+
+import Effect (Effect)
+import Erl.Data.List (nil, (:))
+import Pinto (SupervisorName)
+import Pinto as Pinto
+import Pinto.Sup (SupervisorChildRestart(..), SupervisorChildType(..), buildChild, childId, childRestart, childStart, childType)
+import Pinto.Sup as Sup
+import Rtsv2.Agents.IngestRtmpCrypto as IngestRtmpCrypto
+import Rtsv2.Agents.IngestStats as IngestStats
+import Rtsv2.Names as Names
+
+isAgentAvailable :: Effect Boolean
+isAgentAvailable = Pinto.isRegistered serverName
+
+serverName :: SupervisorName
+serverName = Names.ingestOneForOneSupName
+
+startLink :: forall a. a -> Effect Pinto.StartLinkResult
+startLink _ = Sup.startLink serverName init
+
+init :: Effect Sup.SupervisorSpec
+init = do
+  pure $ Sup.buildSupervisor
+    # Sup.supervisorStrategy Sup.OneForAll
+    # Sup.supervisorChildren
+        ( ( buildChild
+              # childType Worker
+              # childId "ingestRtmpCrypto"
+              # childStart IngestRtmpCrypto.startLink unit
+              # childRestart Transient
+          )
+          : ( buildChild
+              # childType Worker
+              # childId "ingestStatsServer"
+              # childStart IngestStats.startLink unit
+              # childRestart Transient
+          )
+            : nil
+        )
