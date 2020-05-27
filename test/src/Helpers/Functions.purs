e59134ac5e415872196c898a84f2ca4e1be6ee6d
Cmdv <vincent@cmdv.me>
2020-05-27 16:17:33 +0100

    Broadcast message tests for different slots

--- test/src/Helpers/Functions.purs
+++ test/src/Helpers/Functions.purs
@@ -124,22 +124,22 @@ stopSession = do
 
 startSlotHigh1000 :: String -> Aff Unit
 startSlotHigh1000 ip = do
-  _ <- delay (Milliseconds 8000.0)
+  _ <- delay (Milliseconds 5000.0)
   runProc "./scripts/run_slot1_1000.sh" [ip]
 
 start2SlotHigh1000 :: String -> Aff Unit
 start2SlotHigh1000 ip = do
-  _ <- delay (Milliseconds 8000.0)
+  _ <- delay (Milliseconds 5000.0)
   runProc "./scripts/run2_slot1_1000.sh" [ip]
 
 startSlotHigh1000Backup :: String -> Aff Unit
 startSlotHigh1000Backup ip = do
-  _ <- delay (Milliseconds 8000.0)
+  _ <- delay (Milliseconds 5000.0)
   runProc "./scripts/run_slot1b_1000.sh" [ip]
 
 startSlotHigh1000Canary :: String -> Aff Unit
 startSlotHigh1000Canary ip = do
-  _ <- delay (Milliseconds 8000.0)
+  _ <- delay (Milliseconds 5000.0)
   runProc "./scripts/run_slot1_1000.sh" [ip, "1936"]
 
 stopSlot :: Aff Unit
