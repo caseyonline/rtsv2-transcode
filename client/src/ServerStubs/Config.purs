eff9474233c681fb4b90f26c4382af44316cf5fe
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2020-05-29 14:33:27 +0000

    Take support client port as current origin port

--- client/src/ServerStubs/Config.purs
+++ client/src/ServerStubs/Config.purs
@@ -2,12 +2,22 @@ module Rtsv2.Config where
 
 import Prelude
 
+import Data.Int as Int
+import Data.Maybe (fromMaybe)
 import Effect (Effect)
 import Shared.Rtsv2.Types (WebConfig)
+import Unsafe.Coerce (unsafeCoerce)
+import Web.HTML (window)
+import Web.HTML.Location as L
+import Web.HTML.Window as Window
 
--- these need to match what they are in release-files/rtsv2_core.config
+-- Web config stub, this is used in URL construction in the endpoint modules
+-- However the support client only needs the support port, which is its origin port
 webConfig :: Effect WebConfig
-webConfig = pure  { publicPort: 3000
-                  , systemPort: 3001
-                  , supportPort: 3002
-                  }
+webConfig = do
+  originPortStr <- window >>= Window.location >>= L.port
+  let originPort = fromMaybe 80 $ Int.fromString originPortStr
+  pure { publicPort: unsafeCoerce "This should not be used"
+       , systemPort: unsafeCoerce "This should not be used"
+       , supportPort: originPort
+       }
