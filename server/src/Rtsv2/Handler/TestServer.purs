c9fe6370ba614d1b6242e00713a74919f46b0610
Casey MacDonald <caseyonline@github.com>
2020-06-30 16:30:39 -0700

    update with Logger

---
+++ server/src/Rtsv2/Handler/TestServer.purs
@@ -0,0 +1,28 @@
+module Rtsv2.Handler.TestServer
+       (
+         ping
+       ) where
+
+import Prelude
+
+import Rtsv2.Agents.TestNodeServer as TestNodeServer
+import Rtsv2.Config (LoadConfig)
+import Rtsv2.Config as Config
+import Effect (Effect)
+import Effect.Console (log, logShow)
+import Data.Maybe (Maybe(..))
+import Stetson (StetsonHandler)
+import Shared.Rtsv2.Types (CanaryState(..), FailureReason, Server)
+import Shared.Common (Url)
+import StetsonHelper (jsonResponse)
+
+
+ping :: LoadConfig -> CanaryState -> StetsonHandler (Maybe (String))
+ping loadConfig canary =
+  jsonResponse getUrls
+
+  where
+    getUrls =
+      do
+        config <- Config.llnwApiConfig
+        pure $ Just "some string"
